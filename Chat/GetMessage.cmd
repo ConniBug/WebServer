18fb88677ad5d2b57dc0e736b0753020114c5de6
Connor <31339874+EnderstormX@users.noreply.github.com>
2018-12-12 11:36:55 +0000

    Upload Client

---
+++ Chat/GetMessage.cmd
@@ -0,0 +1,33 @@
+@echo off
+title Chat Client
+color 97
+MODE 38,4
+echo report any found bugs to 
+echo           connorbilham9@googlemail.com
+pause
+MODE 27,22
+:a
+cls
+echo ___________________________
+echo      Online Chat                                    
+echo Server Version:           
+echo                  V1.2.0.1 
+echo Client Version:           
+echo                  V1       
+echo Online
+echo ___________________________
+echo Online
+Connections.py
+echo ___________________________
+WebServerGet.py
+WebServerGet.py
+WebServerGet.py
+WebServerGet.py
+WebServerGet.py
+WebServerGet.py
+WebServerGet.py
+ping localhost -n 3 >NUL
+
+goto A:
+
+pause
